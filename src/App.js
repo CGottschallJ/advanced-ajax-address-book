2c5bc2a2afcece06c15c60bda0555a1c4b7bf3bd
CGottschallJ <Cameron.Gottschall@gmail.com>
2018-01-11 16:27:47 -0600

    Final: AJAX to API complete. All data/functionality complete.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { connect } from 'react-redux';
+import {loadUsers} from "./actions";
 import UserDetailContainer from "./containers/UserDetailContainer";
 import ListOfUsersContainer from "./containers/ListOfUsersContainer";
 import SearchBox from "./components/SearchBox";
 
-function App(props) {
-  return (
-    <div>
-      <SearchBox />
-      <ListOfUsersContainer />
-      <UserDetailContainer />
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount(){
+    this.props.loadUsers();
+  }
+  render(){
+    return (
+      <div>
+        <SearchBox />
+        <ListOfUsersContainer />
+        <UserDetailContainer />
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapDispatchToProps(dispatch){
+  return {
+    loadUsers:function(user){
+      dispatch(loadUsers(user));
+    }
+  }
+}
+
+export default connect(null,mapDispatchToProps)(App);
--- src/actions/index.js
+++ src/actions/index.js
@@ -11,3 +11,24 @@ export function setSearchText(text){
     value:text
   }
 }
+
+export function loadUsers() {
+  return function(dispatch) {
+    dispatch({
+      type:"LOAD_USERS"
+    });
+    fetch("/users")
+    .then((response)=>{
+      return response.json();
+    }).then((users) => {
+      dispatch(usersLoaded(users));
+    });
+  }
+}
+
+export function usersLoaded(users) {
+  return {
+    type: "USERS_LOADED",
+    value: users
+  }
+}
